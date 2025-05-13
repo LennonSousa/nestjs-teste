dbfd48148b1a80cd171c06e501a9b098d29a1e72
John Lennon Sousa Costa <lennonsousa@outlook.com>
2025-05-13 15:10:16 -0300

    feat: add transaction http controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TransactionRepository } from '@domain/repositories/TransactionRepository';
+import { InMemoryTransactionRepository } from '@external/repositories/InMemoryTransactionRepository';
+import { CreateTransactionUseCase } from '@useCases/CreateTransactionUseCase';
+import { DeleteAllTransactionsUseCase } from '@useCases/DeleteAllTransactionsUseCase';
+import { GetStatisticsUseCase } from '@useCases/GetStatisticsUseCase';
+import { TransactionController } from '@controllers/http/TransactionController';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { TransactionGateway } from '@controllers/event/TransactionGateway';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 60,
+        },
+      ],
+    }),
+  ],
+  controllers: [TransactionController],
+  providers: [
+    {
+      provide: TransactionRepository,
+      useClass: InMemoryTransactionRepository,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    CreateTransactionUseCase,
+    DeleteAllTransactionsUseCase,
+    GetStatisticsUseCase,
+    TransactionGateway,
+  ],
 })
 export class AppModule {}
