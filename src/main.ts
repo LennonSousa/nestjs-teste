e6685dbaa81fc1480ee8034d3e0976e1edd020a8
John Lennon Sousa Costa <lennonsousa@outlook.com>
2025-05-13 15:10:42 -0300

    feat: add configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { Logger } from '@shared/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(new Logger());
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Transactions API')
+    .setDescription('Create and get statistics from Transactions')
+    .setVersion('1.0')
+    .addTag('Transactions')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
